80fc39d7ed87d4e497b55539681ae1cee2c4c2b7
dani <dbalance0@abv,bg>
2023-04-23 06:29:29 +0300

    Create README.md and add description in Catalog

---
+++ README.md
@@ -0,0 +1,46 @@
+```php
+use PureIntento\PrintifySdk\PrintifyClient;
+use PureIntento\PrintifySdk\Catalog;
+use PureIntento\PrintifySdk\Shop;
+
+/**
+ * Init printifyclient with argument auth token
+ */
+$authToken="YOU_AUTH_TOKEN_HERE";
+$request=new PrintifyClient($authToken);
+
+/**
+ * Създава инстанция на Catalog с аргумент PrintifyClient
+ * Каталога се използва за извличане на данни в каталога на Printify
+ * Create instance on Catalog with argument PrintifyClient instance
+ * The catalog is used to retrieve data in the Printify catalog
+ */
+$catalog=new Catalog($request);
+/**
+ * Създаване на инстанция на Shop с аргумент PrintifyClient инстнация
+ * Shop се използва за извличане на данни за магазините в Printify
+ * 
+ * Create an instance of Shop with argument PrintifyClient instance
+ * Shop is used to retrieve data about stores in Printify
+ */
+$shop=new Shop($request);
+
+
+$blueprintId=3;
+print_r($catalog->blueprint($blueprintId));
+print_r($catalog->blueprints());
+
+$printProviderId=43;
+print_r($catalog->blueprintProviderVariants($blueprintId,$printProviderId));
+print_r($catalog->blueprintProviderShipping($blueprintId,$printProviderId));
+print_r($catalog->printProviders());
+print_r($catalog->printProvider($printProviderId));
+
+
+print_r($shop->shops());
+
+$shopId=2;
+print_r($shop->deleteShop($shopId));
+
+
+```
--- src/Catalog.php
+++ src/Catalog.php
@@ -9,21 +9,33 @@ class Catalog{
         $this->client=$client;
     }
     
+    /**
+     * Retrieve a specific blueprint
+     */
     public function blueprint($id,$options = []) : Structures\Blueprint{
         $array=$this->client->request(APIPath::blueprint($id), "GET", $options)->getArrayResponse();
         return new Structures\Blueprint($array);
     }
 
+    /**
+     * Retrieve a list of all available blueprints
+     */
+
     public function blueprints($options = []) : StructureCollection{
         $array=$this->client->request(APIPath::blueprints(), "GET", $options)->getArrayResponse();
         return new StructureCollection(Structures\Blueprint::class,$array);
     }
-
+    /**
+     * Retrieve a list of all print providers that fulfill orders for a specific blueprint
+     */
     public function blueprintProviders($blueprintId,$options = []) : StructureCollection{
         $array=$this->client->request(APIPath::blueprintProviders($blueprintId), "GET", $options)->getArrayResponse();
         return new StructureCollection(Structures\PrintProvider::class,$array);
     }
-
+    
+    /**
+     * Retrieve a list of all variants of a blueprint from a specific print provider
+     */
     public function blueprintProviderVariants($blueprintId, $printProviderId,$options = []) : StructureCollection{
         $array=$this->client->request(APIPath::blueprintProviderVariants($blueprintId,$printProviderId), "GET", $options)->getArrayResponse();
         if(!array_key_exists("variants",$array)){
@@ -32,6 +44,9 @@ class Catalog{
         return new StructureCollection(Structures\blueprintProviderVariant::class,$array["variants"]);
     }
 
+    /**
+     * Retrieve the shipping information for all variants of a blueprint from a specific print provider
+     */
     public function blueprintProviderShipping($blueprintId, $printProviderId,$options = []) : Structures\BlueprintProviderShipping{
         $array=$this->client->request(APIPath::blueprintProviderShipping($blueprintId,$printProviderId), "GET", $options)->getArrayResponse();
         if(!array_key_exists("profiles",$array)){
@@ -44,11 +59,18 @@ class Catalog{
         return new Structures\BlueprintProviderShipping($array);
     }
 
+
+    /**
+     * Retrieve a list of all available print-providers
+     */
     public function printProviders($options=[]) : StructureCollection{
         $array=$this->client->request(APIPath::printProviders(), "GET", $options)->getArrayResponse();
         return new StructureCollection(Structures\PrintProvider::class,$array);
     }
 
+    /**
+     * Retrieve a specific print-provider and a list of associated blueprint offerings
+     */
     public function printProvider($id,$options=[]) : Structures\PrintProvider{
         $array=$this->client->request(APIPath::printProvider($id), "GET", $options)->getArrayResponse();
         return new Structures\PrintProvider($array);
